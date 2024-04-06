e148946b245871adce72556bc727afaea1cd80ad
Kevin Roberto Perdomo <kevinrp47@gmail.com>
2024-04-06 19:12:18 -0400

    commented out unused pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Blog from './components/Blog/Blog';
-import Resume from './components/Resume/Resume';
+// import Projects from './components/Projects/Projects';
+// import Blog from './components/Blog/Blog';
+// import Resume from './components/Resume/Resume';
 import './styles/App.css';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          {/* <Route path="/projects" element={<Projects />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/resume" element={<Resume />} /> */}
         </Routes>
       </div>
     </Router>
