d906675ed124ed5a8f6ba501936dae77f3b0dd18
Kevin Roberto Perdomo <kevinrp47@gmail.com>
2024-04-06 19:07:11 -0400

    small updates

---
+++ src/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <img src="/assets/images/profile.jpg" alt="Kevin Perdomo" className="profile-image"/>
+      <h1>Hi, I'm Kevin Perdomo</h1>
+      <p>I'm a passionate AI & Machine Learning Engineer with experience in building, implementing, and optimizing cutting-edge technology solutions. Welcome to my portfolio!</p>
+    </div>
+  );
+}
+
+export default About;
