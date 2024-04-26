540cd7de25333462a5e9e031fb75ad99cfdf0c83
Kevin Roberto Perdomo <kevinrp47@gmail.com>
2024-04-26 14:36:46 -0400

    change subtext on image

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="/pf.jpg"
-              alt="Ricardo portrait"
+              alt="Kevin Portrait"
               width="192"
               height="192"
               quality="95"
