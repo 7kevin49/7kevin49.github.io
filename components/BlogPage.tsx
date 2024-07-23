06d83c0ac6b4446e82d76587226cfe4a42efddec
Kevin Roberto Perdomo <kevinrp47@gmail.com>
2024-07-23 16:19:13 -0400

    fixes the dpeloyment issues

--- components/BlogPage.tsx
+++ components/BlogPage.tsx
@@ -1,17 +1,17 @@
 // components/BlogPage.tsx
-import React from 'react';
-import Header from './header';
-import Footer from './footer';
-import ThemeContextProvider from '../context/theme-context';
+// import React from 'react';
+// import Header from './header';
+// import Footer from './footer';
+// import ThemeContextProvider from '../context/theme-context';
 
-const BlogPage = ({ children }) => {
-  return (
-    <ThemeContextProvider>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </ThemeContextProvider>
-  );
-};
+// const BlogPage = ({ children }) => {
+//   return (
+//     <ThemeContextProvider>
+//       <Header />
+//       <main>{children}</main>
+//       <Footer />
+//     </ThemeContextProvider>
+//   );
+// };
 
-export default BlogPage;
+// export default BlogPage;
